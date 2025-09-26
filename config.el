cd8bccbb309ddc85e3187c0cd202ff49db9ddca1
Alexander Koc <alexander.koc@skane.se>
2025-09-26 22:14:06 +0200

    add dired-narrow

--- config.el
+++ config.el
@@ -119,6 +119,10 @@
   :bind (("C-c <f6>" . heaven-and-hell-load-default-theme)
          ("<f6>" . heaven-and-hell-toggle-theme)))
 
+(use-package dired-narrow
+  :ensure t
+  :bind ((:map dired-mode-map
+               ("C-s" . dired-narrow))))
 ;; treemacs workaround thingie:
 ;; (add-to-list 'image-types 'svg)
 
