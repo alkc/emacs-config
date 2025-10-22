49aef09eb1105089d9549c11b161e25a1f07ea20
Alexander Koc <alexander.koc@skane.se>
2025-10-22 21:05:17 +0200

    add some wip build flags

--- build.sh
+++ build.sh
@@ -19,3 +19,23 @@ fi
 target_version_tag_name="emacs-${TARGET_EMACS_VERSION}"
 git fetch --depth 1 origin tag "${target_version_tag_name}"
 git checkout "tags/${target_version_tag_name}"
+
+# WIP and untested:
+./configure.sh
+./autogen.sh \
+    CFLAGS="-march=native -O3 -pipe -fno-finite-math-only" \
+    --with-modules \
+    --with-x-toolkit=lucid \
+    --with-xft \
+    --with-harfbuzz \
+    --with-xaw3d \
+    --with-png --with-jpeg --with-tiff --with-gif --with-webp --with-rsvg \
+    --with-imagemagick \
+    --with-tree-sitter \
+    --with-native-compilation=aot \
+    --without-compress-install \
+    --without-toolkit-scroll-bars \
+    --without-gsettings \
+    --without-gconf \
+    --without-mailutils \
+    --without-sound
