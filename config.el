39997a6ec6214096fb98482aef968c1ea24326dd
Alexander Koc <alexander.koc@skane.se>
2026-09-08 15:42:42 +0200

    include diary?

--- config.el
+++ config.el
@@ -250,6 +250,7 @@
   (setq org-log-done t)
   (setq org-log-refile t)
   (setq org-startup-folded 'fold)
+  (setq org-agenda-include-diary t)
   (setq org-archive-location  "./archive/%s_archive::")
   (setq org-default-notes-file     (concat org-directory "soppa.org"    ))
   (setq +org-capture-notes-file    (concat org-directory "soppa.org"    ))
